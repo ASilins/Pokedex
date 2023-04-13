e5a796cac99b7910b5f36e7f15dd88bd58aea3bb
Artūrs Siliņš <asilins@Arturss-MacBook-Pro.local>
2023-04-13 20:13:52 +0300

    Added pokemon class

---
+++ src/components/Pokemon.js
@@ -0,0 +1,19 @@
+function Pokemon() {
+    return (
+        <>
+            <div>
+                <p>
+                    Number
+                </p>
+                <h1>
+                    Pokemon name
+                </h1>
+            </div>
+            <div>
+                img
+            </div>
+        </>
+    )
+}
+
+export default Pokemon;
